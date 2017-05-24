1af4c6b2cada5872e38531037c0ca701cca382db
J Fixby <github@jfixby.com>
2017-05-24 19:18:27 +0200

    +android

--- android-example-library/src/main/java/jitpack/android/example/JitpackAndroidExampleLib.java
+++ android-example-library/src/main/java/jitpack/android/example/JitpackAndroidExampleLib.java
@@ -1,10 +1,13 @@
+
 package jitpack.android.example;
+
+import android.view.View;
 
 public class JitpackAndroidExampleLib {
 
 	/** Creates a greeting **/
 	public String greet (final String name) {
-// final View x = null;
+		final View x = null;
 		return "Hi! " + name;
 	}
 
